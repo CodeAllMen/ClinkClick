ad53e60e5d01341ab4b9e1e5535629bc56138b03
yy <a1023767856@163.com>
2020-05-21 10:21:20 +0800

    add somethings

--- models/clinklink.go
+++ models/clinklink.go
@@ -5,8 +5,10 @@
 package models
 
 import (
+	"fmt"
 	"github.com/astaxie/beego/logs"
 	"github.com/astaxie/beego/orm"
+	"time"
 )
 
 type ClinkLink struct {
@@ -38,13 +40,36 @@ func (c *ClinkLink) Insert() {
 func (c *ClinkLink) GetListSub(startTime, endTime, startTime2, endTime2 string) (list []*ClinkLink, err error) {
 	db := orm.NewOrm()
 
-	if _, err = db.Raw("select * from clink_link c " +
-		"where c.trans_time>=? and " +
-		"c.trans_time<=? and c.trans_type='RENEW' and c.status=1 and  " +
-		"c.msisdn in(select msisdn from clink_link d " +
+	startTime = changeTime(startTime)
+	endTime = changeTime(endTime)
+
+	if _, err = db.Raw("select * from clink_link c "+
+		"where c.trans_time>=? and "+
+		"c.trans_time<=? and c.trans_type='RENEW' and c.status=1 and  "+
+		"c.msisdn in(select msisdn from clink_link d "+
 		"where d.trans_type='SUB' and d.status=1 and d.trans_time>=? and d.trans_time<=?);", startTime, endTime, startTime2, endTime2).QueryRows(&list); err != nil {
 		// err = libs.NewReportError(err)
 	}
 
 	return
+}
+
+func changeTime(timeStr string) string {
+	var (
+		time2  time.Time
+		err    error
+		result string
+	)
+
+	// time1 := "2020-05-12 11:17:19"
+
+	if time2, err = time.Parse("2006-01-02 15:04:05", timeStr); err != nil {
+		fmt.Println(err)
+	}
+
+	year, mon, day := time2.UTC().Date()
+	hour, min, sec := time2.UTC().Clock()
+	result = fmt.Sprintf("%d-%02d-%02dT%02d:%02d:%02d+08:00", year, mon, day, hour, min, sec)
+
+	return result
 }
